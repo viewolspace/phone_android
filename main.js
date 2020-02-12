453f6a41e20313c76f334314b4a4082bacbbf4e8
gexiaowei <gandxiaowei@gmail.com>
2020-02-12 22:16:29 +0800

    :sparkles: add interface

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App'
+import uniRequest from 'uni-request'
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
+
+uniRequest.defaults.baseURL = 'http://39.106.166.222:8080/phoneWeb'
 
 const app = new Vue({
   ...App
--- pages/tools/query.vue
+++ pages/tools/query.vue
@@ -9,7 +9,7 @@
     </view>
     <view class="keywords">
       <input
-        class="input"
+        class="phone-input"
         v-model="phone_number"
         placeholder-style="color:#838383"
         confirm-type="search"
@@ -18,20 +18,46 @@
       />
     </view>
     <view class="query-container">
-      <button class="query">查询</button>
+      <button class="query" :disabled="!phone_number" @click="getPhoneLocation">
+        查询
+      </button>
     </view>
-    <view class="result text-center">
-      <view>手机号码"{{ phone_number }}" </view>
-      <view>北京中国移动</view>
+    <view class="result text-center" v-if="location">
+      <view>手机号码"{{ result_phone }}" </view>
+      <view>{{ location }}</view>
     </view>
   </view>
 </template>
 
 <script>
+import uniRequest from 'uni-request'
+
 export default {
   data () {
     return {
-      phone_number: '13816212176'
+      phone_number: '',
+      result_phone: '',
+      location: ''
+    }
+  },
+  methods: {
+    async getPhoneLocation () {
+      const {
+        data: { message, status }
+      } = await uniRequest.get('/tool/phoneSelect', {
+        params: { phone: this.phone_number }
+      })
+
+      if (status === '0000') {
+        this.result_phone = this.phone_number
+        this.location = message
+      } else {
+        uni.showToast({
+          icon: 'none',
+          title: message,
+          duration: 2000
+        })
+      }
     }
   }
 }
@@ -40,9 +66,11 @@ export default {
 <style lang="scss">
 .content {
   height: 100%;
+  padding: 0 28upx;
   background: #f6f6f6;
 
   .title {
+    margin: 0 -28upx;
     padding: 25upx;
     color: white;
     font-size: 38upx;
@@ -60,19 +88,19 @@ export default {
   }
 
   .keywords {
-    padding: 0 28upx;
-    .input {
-      background: #ffffff;
+    padding: 20upx 0;
+
+    background: #ffffff;
+    .phone-input {
       color: #282828;
-      padding: 40upx 0;
+      line-height: 80upx;
       font-size: 32upx;
-      color: #282828;
       text-align: center;
     }
   }
   .query-container {
     margin-top: 82upx;
-    padding: 0 28upx;
+
     .query {
       background: #2bb9c1;
       border-radius: 46upx;
